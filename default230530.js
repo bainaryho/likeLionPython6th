2902823377bcf562b62dc6ef82e66418cba3bcab
bainaryho <rkswl233@naver.com>
2023-05-30 13:15:51 +0900

    java script 실습2 변수

--- default230530.js
+++ default230530.js
@@ -1,12 +1,38 @@
-document.write('Hello World');
-document.write('<h1>Welcome to JS program</h1>');
-document.write('<h2>Welcome to JS Program</h2>');
+//document.write('Hello World');
+//document.write('<h1>Welcome to JS program</h1>');
+//document.write('<h2>Welcome to JS Program</h2>');
+//
+//console.log('Welcome JS program');
+//console.info('Welcome JS program');
+//console.warn('Welcome JS program');
+//console.error('error JS program');
+//
+//alert('Welcome JS Program'); //쓰지 말자
+//var a = prompt('Welcome JS Program'); //변수 입력 받기 가능
+//console.log(a);
 
-console.log('Welcome JS program');
-console.info('Welcome JS program');
-console.warn('Welcome JS program');
-console.error('error JS program');
+//console.log(typeof 123);
+//console.log(typeof 123.4);
+//console.log(typeof '하이');
+//console.log(typeof !123);
+//console.log(typeof 'asd');
+//console.log(typeof true);
+//console.log(true, typeof true);
 
-alert('Welcome JS Program'); //쓰지 말자
-var a = prompt('Welcome JS Program'); //변수 입력 받기 가능
-console.log(a);
+//var car
+//console.log(typeof car);
+//car = ''
+//console.log(typeof car);
+//var person = {firstName : 'jin', lastName: 'lee', age: 50, eyeColor: 'blue'};
+//console.log(typeof person, person);
+//person = null;
+//console.log(typeof person, person);
+
+var name = '이승훈';
+var age = 29;
+var cgpa = 3.92;
+var lineBrake = '<br/>'
+
+document.write("이름: "+name+lineBrake);
+document.write("나이: "+age+lineBrake);
+document.write("학점: "+cgpa+lineBrake);
