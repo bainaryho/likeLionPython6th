6a792e43ced22cb30b3a67234e95cebc1ab104c4
bainaryho <rkswl233@naver.com>
2023-05-30 11:22:41 +0900

    java script 실습1

--- default230530.js
+++ default230530.js
@@ -1,1 +1,12 @@
-document.write('Hello World2');
+document.write('Hello World');
+document.write('<h1>Welcome to JS program</h1>');
+document.write('<h2>Welcome to JS Program</h2>');
+
+console.log('Welcome JS program');
+console.info('Welcome JS program');
+console.warn('Welcome JS program');
+console.error('error JS program');
+
+alert('Welcome JS Program'); //쓰지 말자
+var a = prompt('Welcome JS Program'); //변수 입력 받기 가능
+console.log(a);
