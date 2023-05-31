93285e2aa4ddcef7ca5499ae03c270842e7ba61e
bainaryho <rkswl233@naver.com>
2023-05-31 13:24:38 +0900

    java script 7 즉시 실행 함수

--- default230531.js
+++ default230531.js
@@ -1,25 +1,40 @@
-var br = "<br/>"
-// 매개변수가 없는 함수 생성하기
-function hello(){
-    document.write("Hello. function without param"+br);
-}
+//var br = "<br/>"
+//// 매개변수가 없는 함수 생성하기
+//function hello(){
+//    document.write("Hello. function without param"+br);
+//}
+//
+//// 한개의 매개변수가 있는 함수 생성
+//function welcomeMessage(name){
+//    document.write("welcome "+name+br);
+//}
+//
+////두개의 매개변수가 있는 함수 생성
+//function addition(num1, num2){
+//    var sum = num1 + num2;
+//    document.write("addtion is "+ sum + br);
+//}
+//
+//function square(num){
+//    return num * num;
+//}
+//
+//hello();
+//welcomeMessage("이진호");
+//addition(1,3);
+//document.write("square of 5 is " + square(5)+br);
 
-// 한개의 매개변수가 있는 함수 생성
-function welcomeMessage(name){
-    document.write("welcome "+name+br);
-}
+//IIFE 예제
+(function display(message){
+    console.log(message);
+})("hi");
 
-//두개의 매개변수가 있는 함수 생성
-function addition(num1, num2){
-    var sum = num1 + num2;
-    document.write("addtion is "+ sum + br);
-}
+//var display2 = function displayMessage(msg){
+//    console.log(msg)
+//}
+//console.log(typeof display2)
+//display2("display2 message");
 
-function square(num){
-    return num * num;
-}
-
-hello();
-welcomeMessage("이진호");
-addition(1,3);
-document.write("square of 5 is " + square(5)+br);
+(function addNum(a , b){
+    console.log(a + b);
+})(3,5);
