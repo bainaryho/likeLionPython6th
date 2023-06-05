31ed59e140f6a4bbb3df23b32f8dbc5ec0472236
bainaryho <rkswl233@naver.com>
2023-06-05 10:23:04 +0900

    python 변수 실습

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+#변수 선언
+a = 10
+b = 3.14
+c = "Hellow, World!"
+d = [1, 2, 3]
+e = (4, 5, 6)
+f = {7, 8, 9}
+g = {"apple":1, "banana":2, "orange":3}
+
+#데이터 타입 출력
+print("a:", type(a)) #<class 'int'> 정수
+print("b:", type(b)) #<class 'float'> 실수
+print("c:", type(c)) #<class 'str'> 문자열
+print("d:", type(d)) #<class 'list'> 리스트
+print("e:", type(e)) #<class 'tuple'> 순서쌍(튜플)
+print("f:", type(f)) #<class 'set'> 집합
+print("g:", type(g)) #<class 'dict'> 딕셔너리
