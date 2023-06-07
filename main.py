3cfeaba749ba2946860123acb02480e60cd9a9e7
bainaryho <rkswl233@naver.com>
2023-06-07 14:10:07 +0900

    python 문자열 실습

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-# import array
-# stu_roll = array.array('i',[101,102,103,104,105])
-#
-# for e in stu_roll:
-#     print(e)
-#
-from array import *
+str1 = 'like lion'
+str2 = "like lion"
+str3 = '''
+    동해물과
+    백두산이 마르고 닳도록
+    하느님이 보우하사 '''
+str4 = """
+    동해물과
+    백두산이 마르고 닳도록
+    하느님이 보우하사 """
 
-# stu_roll = array('i',[101,102,103,104,105])
-# n = len(stu_roll)
-# i = 0
-#
-# print('array index 메소드')
-# print(stu_roll.index(101)) #0
-#
-# print('extend() 메소드')
-# arr = array('i',[201,208,209])
-# stu_roll.extend(arr)
-# print(stu_roll)
-#
-# print('reverse() 메소드')
-# stu_roll.reverse()
-# print(stu_roll)
+print(str1)
+print(str2)
+print(str3)
+print(str4)
 
-# print('sort()')
-# stu_roll.sort() #sort가 없다네
-# print(stu_roll) #에러
 
-print('slicing')
-stu_roll = array('i',[101,102,103,104,105,106,107])
-print(stu_roll)
-print('0:5',stu_roll[:5])
-print('1:5',stu_roll[1:5])
-print('-3:',stu_roll[-3:]) #마지막 3개
-print('4:',stu_roll[4:]) #4번째부터
-print('2:7:2',stu_roll[2:7:2]) #2번째 103부터 2개씩 건너띄며 7번째인덱스 107까지
+str5 = 'Hello "like Lion" How are u'
+print(str5)
