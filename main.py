0f98ec312658069455847b3987c8aa9fadb924a0
bainaryho <rkswl233@naver.com>
2023-06-07 10:32:47 +0900

    python 조건문 실습

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-#이스케이프문자
-print("He said, \"hi there\"") #He said, "hi there"
-print('It\'s beautiful day!') #It's beautiful day!
+# #while 문
+# while 1:
+#     a = int(input('Enter Menu Number: '))
+#     if a == 0:
+#         break
+#     print("a: ",a)
+# else:
+#     print("else")
+
+# a = 2
+# while a< 20:
+#     print(a)
+#     a+=2
+# print("gg")
+
+# i = 0
+# while True:
+#     i+=1
+#     print(i)
+#     if i == 5:
+#         break
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for i in range(-1, -10, -2):
+#     print(i)
+
+# r = range(5,0,-1)
+# print(type(r))
+
+st = '멋쟁이 사자'
+for ch in st:
+    print(ch)
+else:
+    print("Else")
+print('코드 종료')
