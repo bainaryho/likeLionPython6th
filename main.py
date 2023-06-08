74e2d1f467c6b71353c81f24a1f0e1fc140d9ac8
bainaryho <rkswl233@naver.com>
2023-06-08 10:26:51 +0900

    python 함수3 실습

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-# def disp(sh):
-#     print(type(sh))
-#     print("Disp Function "+sh())
-#     #sh() 이거 실화냐? 진짜 몰랐다. 함수에 함수를 넣어버리는..
-# def show():
-#     return  "Show Func"
-#
-# disp(show)
+print('ㅇ거듭제곱함수')
+def pw(x,y):
+    z = x**y
+    print(z)
+pw(2,5)
 
-def disp():
-    def show():
-        return "Show Func"
-    print('disp')
-    return show()
-s = disp()
-print(s)
-#disp
-#Show Func
+print('ㅇ아규먼트 2개인 함수')
+def show(name,age):
+    print(f"Name:{name} Age:{age}")
+show('이진호','24')
 
+print('함수실습2')
+def add(*num):
+    z = num[0] + num[1] + num[2]
+    print('add : ',z)
+add('5','2','4')
 
-
+print('함수실습3')
+def add(x, *num):
+    z = x + num[0] + num[1]
+    print('Add x *: ',z)
+add(5, 2, 4, 5)
