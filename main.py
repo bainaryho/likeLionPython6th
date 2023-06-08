1713ec9de88969bf689224c1fb220540b644b402
bainaryho <rkswl233@naver.com>
2023-06-08 14:11:28 +0900

    python 제너레이트 실습

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-#list 실습
-fruits = ['apple', 'banana', 'cherry', 'orange']
-vegetables = ['carrot', 'cucumber']
+# #제너레이터 실습
+# def fibonacci_generator(n):
+#     a, b = 0, 1
+#     for _ in range(n):
+#         yield a
+#         a, b = b, a+b
+#
+# fibonacci_sequence = fibonacci_generator(10)
+#
+# for number in fibonacci_sequence:
+#     print(number)
 
-grocery = fruits + vegetables
-print(grocery)
+# def yield_abc():
+#   yield "A"
+#   yield "B"
+#   yield "C"
+#
+# for ch in yield_abc():
+#   print(ch)
 
-numbers = [10, 5, 6, 1, 3]
-numbers.sort()
-print(numbers)
+def generate_alphabat(sl, el):
+    start = ord(sl)
+    end = ord(el)
+    while start <= end:
+        yield chr(start)
+        start += 1
 
-slice_numbers = numbers[1:4]
-print(slice_numbers)
-
-numbers_clone = numbers[:]
-print(numbers_clone)
+runner = generate_alphabat('A','G')
+print(type(runner))
+for l in runner:
+    print(l)
