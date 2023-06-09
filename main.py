5815db0d9fa94e07b125e8466279d028be83c82d
bainaryho <rkswl233@naver.com>
2023-06-09 14:12:30 +0900

    Python file input output 실습3

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-with open('example.txt', 'r+') as f:
-    content = """ where
-    is my
-    game
-    """
-    f.write(content)
+import os
+
+filename = 'example.txt'
+
+# if os.path.isfile(filename):
+#     print(f'{filename}이 존재')
+# else:
+#     print(f'{filename}이 없음')
+
+file_object = open('list_example.txt','w')
+content_list = ['python','java,','c++','javascript']
+
+for i in content_list:
+    file_object.write(i + '\n')
+
+file_object.close()
