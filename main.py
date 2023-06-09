07541109314cee40d83729f30b410ba75f8ae209
bainaryho <rkswl233@naver.com>
2023-06-09 11:21:38 +0900

    Python 상속 종합 실습

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-import time
-from datetime import datetime
-from datetime import date
+class ParentClass:
+    def __init__(self):
+        self.name = 'parent'
+        self.age = 20
+
+    def __str__(self):
+        return f'ParentClass name: {self.name}, number : {self.age}'
+
+    def add_num(self, new_number):
+        print('부모 : ', new_number, '만큼 더해야지')
+        self.age = self.age + new_number
 
 
-print('time.ctime() : ', time.ctime())  # 현재 시간
+class ChildClass(ParentClass):
+    def __init__(self):
+        super().__init__() #이걸 꼭 넣어야함. 정의 해줘야지
+        self.name = 'child' #그래야 이렇게 변경가능, 기본중의 기본
 
-df = datetime(year=2023, month=5, day=5, hour=5, minute=5)
-print('datetime : ', df, '| type(df) : ', type(df))
-now_date = datetime.now()
-print('datetime.now() : ', now_date)
+    def __str__(self):
+        return f'ChildClass name : {self.name}, number:{self.age}'
 
-d = date(year=2023, month=6, day=1)
-print(d) #date만
+    def add_num(self, new_number):
+        print('말 안듣는 자식 : 고정적으로 5 더할건데?')
+        self.age = self.age + 5
 
-today_date = date.today()
-print(today_date)
+parent = ParentClass()
+child = ChildClass()
+print(parent)
+print(child)
+
+parent.add_num(7)
+child.add_num(7)
+print(parent)
+print(child)
