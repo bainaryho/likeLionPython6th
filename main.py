ec29e99f99265818eed1e1bb4b5a2ef177a9a172
bainaryho <rkswl233@naver.com>
2023-06-12 13:17:24 +0900

    Python try-except 실습

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from MyApp.Handlers.text_handler import handle_text
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("Error: Division to zero")
+    else:
+        print(f'result : {result}')
+    finally:
+        print('well done')
 
-input_text = "py package pratice"
-handle_text(input_text)
+# divide(10,0)
+# #Error: Division to zero
+# #well done
+# divide(5,6)
+# #result : 0.8333333333333334
+# #well done
+try:
+    number = int('not a num') #v Error
+    number = 5 + 'not a num' #t Error
+except ValueError:
+    print("v Error")
+except TypeError:
+    print("t Error")
