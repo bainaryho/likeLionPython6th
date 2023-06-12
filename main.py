7243c924fceb44ec935f4728820502747a5c33eb
bainaryho <rkswl233@naver.com>
2023-06-12 14:24:51 +0900

    Python enumerate 실습

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-def divide(x, y):
-    try:
-        result = x / y
-    except ZeroDivisionError:
-        print("Error: Division to zero")
-    else:
-        print(f'result : {result}')
-    finally:
-        print('well done')
+a = ['a1','b2','c3']
+for i in range(len(a)):
+    print(i,a[i])
 
-# divide(10,0)
-# #Error: Division to zero
-# #well done
-# divide(5,6)
-# #result : 0.8333333333333334
-# #well done
-try:
-    number = int('not a num') #v Error
-    number = 5 + 'not a num' #t Error
-except ValueError:
-    print("v Error")
-except TypeError:
-    print("t Error")
+i = 0
+p_list = []
+for n in a:
+    p_list.append([i,n])
+    i += 1
+print(p_list)
+
+for i,v in enumerate(a):
+    print(i,v)
