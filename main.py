265855e0419cca91d46ad66a7e09906eec167429
bainaryho <rkswl233@naver.com>
2023-06-16 16:24:18 +0900

    CodeLion 세렝게티MBTI 사이트 만들기 ver.1

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-class ListNode:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+temperatures = [73,74,75,71,69,72,76,73]
 
-class Stack:
-    def __init__(self):
-        self.head = None
-        self.size = 0
+answer = [0] * len(temperatures)
+stack = [] #인덱스 저장리스트. temperatures의 0인덱스부터 저장함
 
-    def is_empty(self):
-        return self.size == 0
+for i, cur in enumerate(temperatures):
+    # cur(밸류)가 temperatures[stack에서 pop된 인덱스] 보다 크면 반복
+    while stack and cur > temperatures[stack[-1]]: #stack에서 원소는 그래도 pop할떄는 [-1] 사용
+        last = stack.pop() #stack에서 pop된 인덱스
+        answer[last] = i - last
+    stack.append(i) #stack에 인덱스를 append
 
-    def push(self, value):
-        new_node = ListNode(value)
-        new_node.next = self.head
-        self.head = new_node
-        self.size += 1
-
-    def pop(self):
-        if self.is_empty():
-            raise IndexError("pop from an empty stack")
-        value = self.head.value
-        self.head = self.head.next
-        self.size -= 1
-        return value
-
-stack = Stack()
-stack.push(1)
-stack.push(3)
-stack.push(2)
-stack.push(4)
-
-for _ in range(4):
-    print(stack.pop())
+print(answer)
