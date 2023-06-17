4cee1347ce2bb22511c3ba0209aa273ebdd047b9
bainaryho <rkswl233@naver.com>
2023-06-17 19:17:56 +0900

    Python stack 구현 유효한 괄호 실습

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-temperatures = [73,74,75,71,69,72,76,73]
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
 
-answer = [0] * len(temperatures)
-stack = [] #인덱스 저장리스트. temperatures의 0인덱스부터 저장함
+        for char in s:
+            # 스택 마지막 요소 꺼내서 확인, 비어있으면 #반환
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
 
-for i, cur in enumerate(temperatures):
-    # cur(밸류)가 temperatures[stack에서 pop된 인덱스] 보다 크면 반복
-    while stack and cur > temperatures[stack[-1]]: #stack에서 원소는 그래도 pop할떄는 [-1] 사용
-        last = stack.pop() #stack에서 pop된 인덱스
-        answer[last] = i - last
-    stack.append(i) #stack에 인덱스를 append
+            else:
+                stack.append(char)
 
-print(answer)
+        return not stack
