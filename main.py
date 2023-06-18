037e39f4f375f2ce5e86ec6de38b018d0d7a9d32
bainaryho <rkswl233@naver.com>
2023-06-18 21:26:25 +0900

    Python stack 구현 유효한 괄호 실습2

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+#         mapping = {
+#             ')': '(',
+#             '}': '{',
+#             ']': '['
+#         }
+#
+#         for char in s:
+#             # 스택 마지막 요소 꺼내서 확인, 비어있으면 #반환
+#             if char in mapping:
+#                 top_element = stack.pop() if stack else '#'
+#
+#             else:
+#                 stack.append(char)
+#
+#         return not stack
+
 class Solution:
-    def isValid(self, s: str) -> bool:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last_occurrence = {c: i for i, c in enumerate(s)}
         stack = []
-        mapping = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
+        in_stack = set()
 
-        for char in s:
-            # 스택 마지막 요소 꺼내서 확인, 비어있으면 #반환
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
+        for i, c in enumerate(s):
+            if c not in in_stack:
+                while stack and c < stack[-i] and i < last_occurrence[stack[-1]]:
+                    in_stack.remove(stack.pop())
+                stack.append(c)
+                in_stack.add(c)
 
-            else:
-                stack.append(char)
-
-        return not stack
+        return ''.join()
