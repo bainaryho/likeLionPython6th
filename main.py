d374098c6711b06c1265f2791bb93b1f1f5a99ad
bainaryho <rkswl233@naver.com>
2023-06-19 14:23:01 +0900

    Python request 실습

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-# graph = {
-#     'A' : ['B', 'C'],
-#     'B' : ['A', 'D', 'E'],
-#     'C' : ['A', 'F'],
-#     'D' : ['B'],
-#     'E' : ['B' , 'F'],
-#     'F' : ['C', 'E']
-# }
-#
-# visited = set()
-#
-# def dfs(start_node):
-#     stack = [start_node]
-#     while stack:
-#         node = stack.pop()
-#         if node not in visited:
-#             print(node, end=' ')
-#             visited.add(node)
-#             stack.extend(reversed(graph[node]))
-# ex_node = 'A'
-# dfs(ex_node)
+import urllib.request
 
-from collections import deque
-graph = {
-    'A' : ['B', 'C'],
-    'B' : ['A', 'D', 'E'],
-    'C' : ['A', 'F'],
-    'D' : ['B'],
-    'E' : ['B' , 'F'],
-    'F' : ['C', 'E']
-}
+url = 'https://www.google.com'
 
-visited = set() #방문 노드를 저장할 집합
+request = urllib.request.Request(url)
+response = urllib.request.urlopen(request)
 
-def bfs(start_node):
-    queue = deque([start_node])
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            queue.extend(graph[node])
-ex_node = 'A'
-bfs(ex_node)
+html = response.read()
+
+print(html)
