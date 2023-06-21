2ad2e3598405aa7bdbafbeda7623b387555a9fa7
bainaryho <rkswl233@naver.com>
2023-06-21 13:25:27 +0900

    django 투표 앱 urls.py 설정

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from polls.models import Question, Choice
+
 # Register your models here.
+admin.site.register(Question) #모델 클래스를 주면 알아서 긁어서 admin site에 넣어준다
+admin.site.register(Choice)
